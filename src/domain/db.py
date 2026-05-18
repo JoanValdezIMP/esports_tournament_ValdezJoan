f49060304fe2f1c8cd2a2e90a5d258577b0b7e0a
Joan Valdez Jimenez <joan.valdez@uvic.cat>
2026-05-18 18:00:17 +0200

    Added get_uow factory

--- src/domain/db.py
+++ src/domain/db.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker, Mapped, mapped_column
 from datetime import datetime
 from .config import DB_URL
 
+from repositories import UnitOfWorkFactory
 
 # print(f"Intentando conectar a la base de datos con URL: {DB_URL}")
 
@@ -20,15 +21,9 @@ class Base(DeclarativeBase):
         onupdate=datetime.now, 
         nullable=True
     )
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-# # Pegamos la URL aquí directamente
-# DB_URL = "postgresql+psycopg2://avnadmin:AVNS_RbP8a8oY-xsdJz4LhUc@pg-250affac-joan1234.d.aivencloud.com:14173/esports_torunament?sslmode=require"
-# # src/domain/db.py
-# engine = create_engine(DB_URL, echo=True)
-# # engine = create_engine(DB_URL)
-# Session = sessionmaker(bind=engine)
 
-# class Base(DeclarativeBase):
-#     pass
+uow_factory = UnitOfWorkFactory(DB_URL)
+
+def get_uow():
+    return uow_factory.create()
